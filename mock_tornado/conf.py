cfa64683f8e1574bcfb95a0910f43a95a480b71e
qiulijun <qiulijun@yiqiniu.com>
2019-07-16 15:13:23 +0800

    Revert "1.0.4版本提交,根据tornado模块增加mockserver功能"
    This reverts commit 95d615f

---
+++ mock_tornado/conf.py
@@ -0,0 +1,23 @@
+import os
+
+setting = {
+    "static_path" :os.path.join(os.path.dirname(__file__),"static"),
+    "template_path": os.path.join(os.path.dirname(__file__),"template"),
+    "cookie_secret":"S4aBKI5uT1OBRbRtqMJyCPmd1OmlHUeVvQWDuFK4axE=",
+    "xsrf_cookies":False,
+    "debug":True
+}
+
+
+mysql_options = {
+	"host": "192.168.1.1",
+    "user": "root",
+	"password": "111111",
+	"database": "test"
+}
+
+
+redis_options = {
+    "host" :"192.168.1.1",
+    "port" : 6379
+}
