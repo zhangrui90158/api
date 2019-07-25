051c8bf622e86b90dfc86c03e6109710e88d456a
qiulijun <qiulijun@yiqiniu.com>
2019-07-25 14:14:04 +0800

    增加mysql、redis配置数据

--- config/config.py
+++ config/config.py
@@ -2,7 +2,7 @@ import logging
 import os
 import time
 
-
+"""项目主路径、格式化时间年月日_时分秒"""
 prj_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 today = time.strftime('%Y%m%d',time.localtime())
 now = time.strftime('%Y%m%d_%H%M%S',time.localtime())
@@ -13,7 +13,7 @@ data_path = os.path.join(prj_path,"data")
 log_file = os.path.join(prj_path,"log","log.txt")
 # report_file = os.path.join(prj_path,"report","report1.html")
 report_file = os.path.join(prj_path, 'report','report_{}.html'.format(now))  # 也可以每次生成新的报告
-test_case_path = os.path.join(prj_path,"test\case")
+test_case_path = os.path.join(prj_path,"test","case")
 file_path = os.path.join(prj_path,"debug","test.png")
 
 #log配置
@@ -32,6 +32,23 @@ db_user = 'root'
 db_passwd = '11111'
 db = 'test'
 
+
+"""mysql服务器配置"""
+mysql_options = {
+	"host": "192.168.1.16",
+    "port":3306,
+    "user": "root",
+	"password": "123456",
+	"database": "itcast",
+    "charset":"utf8"
+}
+
+
+"""redis服务器配置"""
+redis_options = {
+    "host" :"192.168.1.16",
+    "port" : 6379
+}
 
 #是否发送邮件配置
 send_email_label= False
